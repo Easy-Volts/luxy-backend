bbdad6a049b2a126fff14d112b5a3fe851cc8ea5
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-08-07 06:56:35 +0100

    fixes: code refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { AuthModule } from './web/auth/module/auth.module';
 import { NotificationModule } from './email-notification/notification.module';
 import { UserModule } from './web/user/module/user.module';
 import { BookingModule } from './web/booking/module/booking.module';
-import { DebugModule } from './web/debug/debug.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -24,7 +23,6 @@ import { ConfigModule } from '@nestjs/config';
     NotificationModule,
     UserModule,
     BookingModule,
-    DebugModule,
   ],
 })
 export class AppModule {
