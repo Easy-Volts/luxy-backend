0a3cdded10421815fa782899a289796b1571dba3
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-10-23 13:27:51 +0100

    feat(auth): extend AuthService interface for driver endpoints

--- src/web/auth/interface/auth.service.ts
+++ src/web/auth/interface/auth.service.ts
@@ -3,10 +3,16 @@ import { ApiResponses } from 'src/dtos/response';
 import { LoginDto } from 'src/dtos/user.auth.dto';
 import { CreateAccountDto } from 'src/dtos/user.createdto';
 import { VerifyOTPDto } from 'src/dtos/verify.otp.request';
+import {
+  DriverRegistrationDto,
+  DriverLoginDto,
+} from 'src/dtos/driver.auth.dto';
 export const AUTH_SERVICE = Symbol('AUTH_SERVICE');
 export interface AuthService {
   createAccount(data: CreateAccountDto): Promise<ApiResponses<any>>;
   resendOTP(data: ResendOTPDto): Promise<ApiResponses<any>>;
   verifyOTP(data: VerifyOTPDto): Promise<ApiResponses<any>>;
   loginUser(loginDto: LoginDto): Promise<ApiResponses<any>>;
+  registerDriver(data: DriverRegistrationDto): Promise<ApiResponses<any>>;
+  loginDriver(loginDto: DriverLoginDto): Promise<ApiResponses<any>>;
 }
