e3a48578e6c07bf86c3d7ff18a86fd3b74efe6b0
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-10-24 02:06:03 +0100

    feat(driver): add driver ratings and statistics API endpoints

--- src/web/drivers/module/driver.module.ts
+++ src/web/drivers/module/driver.module.ts
@@ -9,12 +9,14 @@ import { DriverRepository } from 'src/domain/repository/driver.repository';
 import { CustomLogger } from 'src/log/logs.service';
 import { CarLendingRepository } from 'src/domain/repository/car.booking.repository';
 import { CarLending } from 'src/domain/entities/car.lending.model';
+import { ReviewRepository } from 'src/domain/repository/review.repository';
+import { Review } from 'src/domain/entities/review.model';
 import { SharedModule } from 'src/shared/shared.module';
 import { RolesGuard } from 'src/commons/security/roles.guard';
 import { AuthGuard } from 'src/commons/security/guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Driver, CarLending]), SharedModule],
+  imports: [TypeOrmModule.forFeature([Driver, CarLending, Review]), SharedModule],
   controllers: [DriverController],
   providers: [
     {
@@ -23,6 +25,7 @@ import { AuthGuard } from 'src/commons/security/guard';
     },
     DriverRepository,
     CarLendingRepository,
+    ReviewRepository,
     CustomLogger,
     RolesGuard,
     AuthGuard,
